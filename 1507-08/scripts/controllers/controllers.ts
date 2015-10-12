b8f7b2adcf883b9ffc6d207f1fb05b99e719c636
KenjiMorita <kennjimoritata@yahoo.co.jp>
2015-10-12 17:22:02 +0900

    form validation,ui-validation implements

--- 1507-08/scripts/controllers/controllers.ts
+++ 1507-08/scripts/controllers/controllers.ts
@@ -1,23 +1,19 @@
 /// <reference path="../../DefinitelyTyped-master/angularjs/angular.d.ts"/>
 /// <reference path="../../DefinitelyTyped-master/jquery/jquery.d.ts"/>
 
-	 class MyController{
-		JsonData : any;
-		bizdata : any;
-		fafaData : any;
-		sum : any;
-		constructor(
-
-		){
-			this.fafaData= 'morita';
-			console.log(this.fafaData);
-		}
+class MyController{
+	public fafa:string;
+	public efafa:string;
+	public myForm;
+	public user:{efafa:string;};
+	constructor(
+	){}
 		getdata(){
-			console.log(this.bizdata);
+			console.log(this.fafa);
+		}
+		public validateCorporateIdPattern(value:string){
+			alert(value);
 		}
 	};
-
-		var myApp = angular.module('myApp', []);
-		myApp.controller('MyController', function(){new MyController()});
-		// fafa.controller('Child',Child);
-		// fafa.controller('Mago',Mago);
+	var myApp = angular.module("myApp", ["ngMessages","ui.validate"]);
+	myApp.controller("MyController", function(){new MyController();});
